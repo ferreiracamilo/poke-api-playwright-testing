a8a685fc4f7bcbc11b7790497d459984da884a43
ferreiracamilo <camiloferreira2009@hotmail.com>
2025-05-29 12:14:30 -0300

    Add new test case file and initial test describe

---
+++ tests/berries.spec.js
@@ -0,0 +1,11 @@
+import { test, expect, request } from '@playwright/test';
+
+test.describe('GET /berry/', () => {
+    test.describe('GET /pokemon/ - Positive Cases', () => {
+
+    });
+
+    test.describe('GET /pokemon/ - Negative Cases', () => {
+
+    });
+});
